cda3b7b156db0892dc34d512a52eab2d12277cac
Algirdasbag <75266218+Algirdasbag@users.noreply.github.com>
2021-03-14 23:15:26 +0200

    Add files via upload

---
+++ Valiuta/JS/valiutuKursai.js
@@ -0,0 +1,27 @@
+let valiutuKursai = [
+{
+    valiutosPavadinimas: "Euras €",
+    valiutosTr: "EUR",
+    kursas: 1
+},
+{
+    valiutosPavadinimas: "JAV doleris $",
+    valiutosTr: "USD",
+    kursas: 1.20
+},
+{
+    valiutosPavadinimas: "Didžiosios Britanijos svaras £",
+    valiutosTr: "GBP",
+    kursas: 0.86
+},
+{
+    valiutosPavadinimas: "Kinijos juanis CNY",
+    valiutosTr: "CNY",
+    kursas: 7.76
+},
+{
+    valiutosPavadinimas: "Rusijos rublis RUB",
+    valiutosTr: "RUB",
+    kursas: 87.57
+}
+];
